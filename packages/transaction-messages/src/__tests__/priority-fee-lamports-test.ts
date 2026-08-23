9b4dbc123027a9903f0375e87eecdcd4cd88cc90
Akash Chakraborty <akashchakraborty694@gmail.com>
2026-08-24 00:24:36 +0530

    Update HEAP_SIZE_A to 30,000 in transaction message tests

--- packages/transaction-messages/src/__tests__/priority-fee-lamports-test.ts
+++ packages/transaction-messages/src/__tests__/priority-fee-lamports-test.ts
@@ -7,7 +7,7 @@ import {
 import { TransactionMessage } from '../transaction-message';
 
 const COMPUTE_UNIT_LIMIT_A = 200_000;
-const HEAP_SIZE_A = 32_768; // 32 KiB
+const HEAP_SIZE_A = 30_000;
 
 const PRIORITY_FEE_LAMPORTS_A = 5_000n;
 const PRIORITY_FEE_LAMPORTS_B = 10_000n;
